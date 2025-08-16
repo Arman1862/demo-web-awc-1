f14ed762dea1adf23d68c5d9c0585b7411c0d89b
Arman4102 <pinil1862@gmail.com>
2025-08-16 07:25:18 +0700

    5

--- src/components/JadwalCard.jsx
+++ src/components/JadwalCard.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 function JadwalCard({ hari, daftarIsi }) {
   return (
     <div className="glass-box py-1 text-center mx-2" tabIndex="0">
-      <h4 className="text-white fw-bold mb-2">{hari}</h4>
+      <h4 className="text-white fw-bold mb-0">{hari}</h4>
       <ul className="text-white list-unstyled small fs-6" style={{marginBottom: '0px'}}>
         {daftarIsi.map((pel, index) => (
           <li key={index}>{pel}</li>
